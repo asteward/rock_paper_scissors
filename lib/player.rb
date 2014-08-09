eb53f3be8531338710d951574a30352651cd4400
Austin Steward <austinsteward@aol.com>
2014-08-09 05:33:39 +0000

    Compare player\'s hands for winner

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,36 @@
 class Player
-  attr_reader = :type
+  attr_reader :hand
   def initialize(hand)
     @hand = hand
+  end
+
+  def compare_hands(other_player)
+    winner = "TIE"
+    if self.hand == "ROCK"
+      if other_player.hand == "ROCK"
+        winner = "TIE"
+      elsif other_player.hand == "PAPER"
+        winner = other_player
+      else
+        winner = self
+      end
+    elsif self.hand == "PAPER"
+      if other_player.hand == "ROCK"
+        winner = self
+      elsif other_player.hand == "PAPER"
+        winner = "TIE"
+      else
+        winner = other_player
+      end
+    else
+      if other_player.hand == "ROCK"
+        winner = other_player
+      elsif other_player.hand == "PAPER"
+        winner = self
+      else
+        winner = "TIE"
+      end
+    end
+    winner
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,4 +8,12 @@ describe 'Player' do
       expect(new_player).to be_an_instance_of Player
     end
   end
+
+  describe 'compare_hands' do
+    it 'returns the winner of rock-paper-scissors game between two players' do
+      player = Player.new("ROCK")
+      player2 = Player.new("PAPER")
+      expect(player.compare_hands(player2)).to eq player2
+    end
+  end
 end
