ca2809342cb34ac434a8ad9ef3ecf2c8e85b23a5
Austin Steward <austinsteward@aol.com>
2014-08-10 22:57:36 +0000

    Add player name to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 class Player
-  attr_reader :hand
-  def initialize(hand)
+  attr_reader :name, :hand
+  def initialize(name, hand)
+    @name = name
     @hand = hand
   end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,37 +4,37 @@ require 'player'
 describe 'Player' do
   describe 'initialize'do
     it 'initializes a Player object' do
-      new_player = Player.new("ROCK")
+      new_player = Player.new("PLAYER ONE","ROCK")
       expect(new_player).to be_an_instance_of Player
     end
   end
 
   describe 'hand_check' do
     it 'checks for a valid hand choice' do
-      new_player = Player.new("ROCK")
+      new_player = Player.new("PLAYER ONE","ROCK")
       expect(new_player.hand_check).to be true
     end
 
     it 'checks for a valid hand choice' do
-      new_player = Player.new("PAPER")
+      new_player = Player.new("PLAYER ONE","PAPER")
       expect(new_player.hand_check).to be true
     end
 
     it 'checks for a valid hand choice' do
-      new_player = Player.new("SCISSORS")
+      new_player = Player.new("PLAYER ONE","SCISSORS")
       expect(new_player.hand_check).to be true
     end
 
     it 'checks for a valid hand choice' do
-      new_player = Player.new("LASER BEAM")
+      new_player = Player.new("PLAYER ONE","LASER BEAM")
       expect(new_player.hand_check).to be false
     end
   end
 
   describe 'compare_hands' do
     it 'returns the winner of rock-paper-scissors game between two players' do
-      player = Player.new("ROCK")
-      player2 = Player.new("PAPER")
+      player = Player.new("PLAYER ONE","ROCK")
+      player2 = Player.new("PLAYER TWO","PAPER")
       expect(player.compare_hands(player2)).to eq player2
     end
   end
