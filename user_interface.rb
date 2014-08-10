02e949780bcfe350da4799caf8f3e0169095d2d8
Austin Steward <austinsteward@aol.com>
2014-08-10 23:59:47 +0000

    Add game menu

--- user_interface.rb
+++ user_interface.rb
@@ -16,8 +16,8 @@ end
 
 def main_menu
   header
-  puts "      N > New Game"
-  puts "      E > Exit"
+  puts "  N > New Game"
+  puts "  E > Exit"
   main_choice = gets.chomp.upcase
   case(main_choice)
   when 'N'
@@ -26,10 +26,30 @@ def main_menu
     puts "Have an excellent day!"
     exit
   else
-    puts "    \"#{main_choice}\" is not a valid option. Please try again."
+    puts "  \"#{main_choice}\" is not a valid option. Please try again."
     sleep 2
   end
   main_menu
+end
+
+def game_menu
+  header
+  puts "  N > New Round"
+  puts "  V > View Game Win Stats"
+  puts "  R > Return to Main Menu"
+  choice = gets.chomp.upcase
+  case(choice)
+  when 'N'
+    new_round
+  when 'V'
+    view_stats
+  when 'R'
+    main_menu
+  else
+    puts "  \"#{choice}\" is not a valid option. Please try again."
+    sleep 2
+  end
+  game_menu
 end
 
 main_menu
